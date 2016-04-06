4e51f4565349cdc95dcbcf0020b4525ea2c4fcd1
Daniel <daniel.hermansson@gmail.com>
2016-04-06 20:13:53 +0200

    en webb

---
+++ KalenderWeb/Controllers/KalenderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KalenderWeb.Controllers
+{
+  public class KalenderController : Controller
+  {
+    // GET: Kalender
+    public ActionResult Index()
+    {
+      var bytes = icalParser.ICalCreator.CreateIcal();
+      var contentType = "text/calendar";
+      return File(bytes, contentType, "hmvblg.ical");
+    }
+  }
+}
--- icalParser/ICalCreator.cs
+++ icalParser/ICalCreator.cs
@@ -1,41 +1,40 @@
 ﻿using DDay.iCal;
-using DDay.iCal.Serialization;
+using DDay.iCal.Serialization.iCalendar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using DDay.iCal.Serialization.iCalendar;
-using System.Net;
 
 namespace icalParser
 {
-  class Program
+  public static class ICalCreator
   {
-    static void Main(string[] args)
+    public static byte[] CreateIcal()
     {
       iCalendar iCal = new iCalendar();
       WebClient webClient = new WebClient();
       webClient.Encoding = Encoding.UTF8;
-      string[] items = webClient.DownloadString("http://dahermansson.github.io/ical/live.txt").Split('$');
+      string[] items = webClient.DownloadString("http://dahermansson.github.io/ical/test.txt").Split('$');
 
       foreach (string item in items)
       {
         if (items.Length == 0)
           continue;
         var parsedItem = ItemBL.CreateItem(item);
-        if(parsedItem != null)
+        if (parsedItem != null)
           iCal.Events.Add(ItemBL.ToEvent(parsedItem));
       }
-        
+
       iCalendarSerializer serializer = new iCalendarSerializer();
       string result = serializer.SerializeToString(iCal);
 
       byte[] bytes = Encoding.UTF8.GetBytes(result);
 
-      File.Create("live.ics").Write(bytes, 0, bytes.Length);
+      return bytes;
     }
-    
+
+
   }
 }
